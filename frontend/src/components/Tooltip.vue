df50d717013186b7f30b68144d0ce1dc794b21c3
Daan Gunnink <github@kreukelz.one>
2025-04-27 14:23:59 +0200

    Show tooltip on keyboard focus

--- frontend/src/components/Tooltip.vue
+++ frontend/src/components/Tooltip.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="group relative flex" :aria-label="label">
-        <div class="absolute pointer-events-none bg-base-300 w-max rounded-md py-1 px-2 line-clamp-1 group-hover:opacity-100 border-black/30 border opacity-0 self-center transition-opacity duration-300 delay-0 group-hover:delay-1000 shadow-md max-h-10 flex items-center justify-center" :class="placementClass">
+        <div class="absolute pointer-events-none bg-base-300 w-max rounded-md py-1 px-2 line-clamp-1 group-hover:opacity-100 border-black/30 group-focus-within:opacity-100 border opacity-0 self-center transition-opacity duration-300 delay-0 group-hover:delay-1000 shadow-md max-h-10 flex items-center justify-center" :class="placementClass">
             <slot name="content" /> <div class="text-xs ml-2">{{ label }}</div>
         </div>
         <slot />
